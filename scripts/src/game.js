c950f8e4f33233dde7a394a852a77b2eee1043f3
jeremyiden <jiden2@illinois.edu>
2016-04-03 07:46:01 -0500

    pure html mode

--- scripts/src/game.js
+++ scripts/src/game.js
@@ -1,11 +1,20 @@
 Game = {
     start: function(){
         Crafty.init(480, 320);
-        Crafty.background('green');
+        Crafty.background('white');
 
-        Crafty.e('2D, DOM, Color, Fourway')
-            .attr({x: 0, y: 0, w: 100, h: 100})
-            .color("#696969")
-            .fourway(200);
+        Crafty.sprite(128, "images/magic.png", {
+            wizard: [0,0,0,0]
+        });
+
+        var iso = Crafty.isometric.init(128);
+        var tile1 = Crafty.e("2D, DOM, wizard, Mouse")
+            .attr("z", 1)
+            .areaMap([64,0],[128,32],[128,96],[64,128],[0,96],[0,32])
+            .bind("click", function(e){
+                if(e.button == 2){
+                    this.destroy();
+                }
+            });
     }
 };
