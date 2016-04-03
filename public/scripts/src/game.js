e33a939511ec7eb2c004b39dbfc52fac737fd249
jeremyiden <jiden2@illinois.edu>
2016-04-03 07:29:06 -0500

    dank shit

--- public/scripts/src/game.js
+++ public/scripts/src/game.js
@@ -2,5 +2,10 @@ Game = {
     start: function(){
         Crafty.init(480, 320);
         Crafty.background('green');
+
+        Crafty.e('2D, DOM, Color, Fourway')
+            .attr({x: 0, y: 0, w: 100, h: 100})
+            .color("#696969")
+            .fourway(200);
     }
 };
