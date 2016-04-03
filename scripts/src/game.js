251dcdd52a8621a7fabc026a8674d858dc0a8833
jeremyiden <jiden2@illinois.edu>
2016-04-03 08:00:41 -0500

    pure html mode

--- scripts/src/game.js
+++ scripts/src/game.js
@@ -12,9 +12,8 @@ Game = {
         });
 
         var iso = Crafty.isometric.size(128);
-        var tile1 = Crafty.e("2D, DOM, dirt, Mouse")
+        var tile1 = Crafty.e("2D, DOM, dirt_tile, Mouse")
             .attr("z", 1)
-            .areaMap([64,0],[128,32],[128,96],[64,128],[0,96],[0,32])
             .bind("click", function(e){
                 if(e.button == 2){
                     this.destroy();
